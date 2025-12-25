927dc85b28ccfa99922b6993866e2f4f10f1dd8a
Rowan Stein <rowan.stein@agyn.io>
2025-12-25 15:31:57 +0000

    Tests: allow trailing-dot FQDN (example.com.) to prepare successfully.\n\nAddress review feedback on PR #14. Task ID: 296

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -2465,6 +2465,10 @@ class TestPreparingURLs(object):
             (
                 u'http://[1200:0000:ab00:1234:0000:2552:7777:1313]:12345/',
                 u'http://[1200:0000:ab00:1234:0000:2552:7777:1313]:12345/'
+            ,
+            (
+                u'http://example.com.',
+                u'http://example.com./'
             )
         )
     )
